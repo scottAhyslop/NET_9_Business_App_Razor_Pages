caeca3ed47afe601b1ba7fe30df33bd03a13ee90
Scott A. Hyslop <sahyslop@gmail.com>
2025-05-05 18:04:37 -0400

    Created _Layout Page and with navbar and implemented across all Pages
    Created Shared Folder at Root of Pages dir
    Created _Layout.cshtml within Shared folder
    Copied previous _Layout View code from MVC project and placed into new _Layout.cshtml
    Implemented new Layout file in all Pages
    Brought in favicon

---
+++ Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_9_Business_App_Razor_Pages.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
