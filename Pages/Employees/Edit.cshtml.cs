8f152e8fae0b2e55cc13055cdde3aed0477cd99e
Scott A. Hyslop <sahyslop@gmail.com>
2025-05-05 16:33:23 -0400

    Added form fields and ModelBinding to Edit Page
    Added form fields in Edit.cshtml and bound them with an InputModel to the form and Employee Model
    Tested and confirmed working with browser, experiencing expected results
    Added rudimentary Validation check on Employee Model in OnPostSave()
    Added error handling to PageModel and created area on Edit Page to display information from the Model, if any

--- Pages/Employees/Edit.cshtml.cs
+++ Pages/Employees/Edit.cshtml.cs
@@ -1,15 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
-namespace NET_9_Business_App_Razor_Pages.Pages.Employees
+namespace NET_9_Business_App_Razor_Pages.Pages
 {
-    public class EditModel : PageModel
+
+    public class EmployeesModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public int? EmployeeId { get; set; }
+
+        [BindProperty]
+        public InputModel? Employee { get; set; }
+
+        public class InputModel
+        {
+            public int? EmployeeId { get; set; }
+            [Required(ErrorMessage = "First name is required.")]
+            public string? EmployeeFirstName { get; set; }
+            [Required(ErrorMessage = "Last name is required.")]
+            public string? EmployeeLastName { get; set; }
+            public string? EmployeePosition { get; set; }
+            public string? EmployeeDepartment { get; set; }
+            public double? EmployeeSalary { get; set; }
+            public List<string>? ErrorMessages { get; set; }
+        }
+
         public void OnGet()
         {
         }
 
-        public void OnPostSave() { }
+        public void OnPostSave()
+        {
+            if (!ModelState.IsValid)
+            {
+                this.Employee!.ErrorMessages = ShowErrors();
+            }
+            //do something when the state is valid
+        }
 
-        public void OnPostDelete() { }
+        public void OnPostDelete()
+        {
+            
+        }
+        //Error display method
+        private List<string> ShowErrors() 
+        { 
+            List<string> errorMessages = new List<string>();
+            foreach (var value in ModelState.Values)
+            {
+                foreach (var error in value.Errors)
+                {
+                    errorMessages.Add(error.ErrorMessage);  
+                }
+            }
+            return errorMessages;
+        }//end ShowErrors
+
     }
 }
