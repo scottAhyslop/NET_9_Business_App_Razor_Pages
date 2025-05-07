cab87b027c68a3068773a59c2eb3b53bf2ab8e8e
Scott A. Hyslop <sahyslop@gmail.com>
2025-05-07 11:43:16 -0400

    Added Employee ViewComponent to project
    Added further UI elements from previous project and created EmployeeViewComponent in Employee Pages
    Created Default.cshtml in ViewComponent and modified pages to accept the new components

--- Pages/Components/EmployeeViewComponent/Default.cshtml.cs
+++ Pages/Components/EmployeeViewComponent/Default.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace NET_9_Business_App_Razor_Pages.Pages.Shared
+namespace NET_9_Business_App_Razor_Pages.Pages.Components.EmployeeComponent
 {
-    public class _LayoutModel : PageModel
+    public class DefaultModel : PageModel
     {
         public void OnGet()
         {
---
+++ Pages/Components/EmployeeViewComponent/EmployeeViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NET_9_Business_App_Razor_Pages.Pages.Components.EmployeeComponent
+{
+    [ViewComponent(Name = "EmployeeViewComponent")]
+    public class EmployeeViewComponent : ViewComponent
+    {
+        
+        public IViewComponentResult Invoke(string message)
+        {
+            // You can pass data to the view using ViewData
+            ViewData["message"] = $"Your message is {message}";
+            return View();
+        }
+    }
+}
